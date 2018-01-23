fce17b33b40b1dff7023384ce03253e10bc6479b
Héctor Astorga <hm.astorga@gmail.com>
2018-01-23 16:28:06 -0300

    Conexión con la BD en el servidor de Johann

--- api/models/nivel.js
+++ api/models/nivel.js
@@ -6,10 +6,24 @@ const Schema = mongoose.Schema;
 
 
 // realizando conexion con la bd
-mongoose.connect('mongodb://localhost/niveles'); // CONEXIÓN LOCAL 
+//mongoose.connect('mongodb://localhost/niveles'); // CONEXIÓN LOCAL 
+
 
 //mongoose.connect('mongodb://Rafiki:#Zeus2018@54.233.193.162/niveles');
 
+var username = 'Rafiki';
+var password = encodeURIComponent('#Zeus2018');
+var database = 'admin';
+var port = '27017';
+var host = '54.233.193.162';
+
+// Añadir conexion con MongoDB aqui
+mongoose.connect(`mongodb://${username}:${password}@${host}:${port}/${database}`, (err, res) => {
+    if(err) {
+        return console.log(`Error al conectarse a la BD: ${err}`);
+    }
+    console.log('Conexion con la BD OK...!');
+});
 
 
 const NivelSchema = Schema({
