6d3c5f436d7eb2dd184c753ce2a4e90da28d41d9
Unknown <by.darklancer@gmail.com>
2018-01-23 12:38:07 -0300

    mofidicacion de moles

--- api/models/asignatura.js
+++ api/models/asignatura.js
@@ -1,11 +1,30 @@
+'use strict';
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+// realizando conexion con la bd
+mongoose.connect('mongodb://localhost/pruebas');
 
 const AsignaturaSchema = new Schema ({
-    id: String, // Codigo de cada establecimiento
+    //id: String, // Codigo de cada establecimiento
     nombre: String,
-    descripcion: String,
-    profesores:[], // puede tener mas de un profesor si el establecimiento lo ve necesario
+    enfasis: [],
+    //descripcion: String,
+    horasPedagogicasConJecAnual: Number,
+    horasPedagogicasSinJecAnual: Number,
+    horasPedagogicasConJecSemanal: Number,
+    horasPedagogicasSinJecSemanal: Number,
+    //profesores:[], // puede tener mas de un profesor si el establecimiento lo ve necesario
 });
 
 module.exports = mongoose.model('Asignatura', AsignaturaSchema);
+
+
+// app.get('/pruebas', (req, res) => {
+//     ModelOrganizacion.find({}, (err, organizacion) => {
+//         console.log(organizacion);
+//         if(err) return res.status(500).send({message: 'Error al realizar peticion: ${err}'});
+//         if(!organizacion) return res.status(400).send({message: 'No existe ninguna organizacion'});
+
+//         res.status(200).send({organizacion});
+//     });
+// });
