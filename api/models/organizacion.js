6fa72f306c8f1f913f5c1f8f271657c86092cf25
Unknown <ogasvega.israel@gmail.com>
2018-01-23 15:55:18 -0300

    Pequeños arreglos
    Cambio de la conexion de la base de datos y adicion de un controlador

--- api/models/organizacion.js
+++ api/models/organizacion.js
@@ -3,35 +3,50 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+var username = 'Rafiki';
+var password = encodeURIComponent('#Zeus2018');
+var database = 'admin';
+var port = '27017';
+var host = '54.233.193.162';
+
+
+// Añadir conexion con MongoDB aqui
+mongoose.connect(`mongodb://${username}:${password}@${host}:${port}/${database}`, (err, res) => {
+    if(err) {
+        return console.log(`Error al conectarse a la BD: ${err}`);
+    }
+    console.log('Conexion con la BD OK...!');
+});
+
 const OrganizacionSchema = new Schema ({
-    rbd: Number, // Codigo de cada establecimiento
-    nombre: String,
-    descripcion: String, 
-    reconocimientoOficial: Date, // Fecha
-    dependencia: String, // Subvencionado, Municipal, etc
-    sostenedor: String, // Corporacion o Sociedad sostenedora
-    orientacionReligiosa: String,
-    direccion: {
-        calle: String,
-        region: String,
-        comuna: String
-    },
-    correo: String,
-    telefono: String,
-    web: String,
-    director: String,
-    fechaPostulacion:{
-        inicio: Date,
-        cierre: Date
-    },
-    mensualidad: Number,
-    totalAlumnosMatriculados: Number,
-    promAlumnosCurso: Number,
-    puntajeSimce: Number,
-    proyectosEducativos:[],
-    nivelEnsenanza: [], // Media, Basica, PreKinder, etc
-    usuarios: [],
-    cursos: []
+    // rbd: Number, // Codigo de cada establecimiento
+    nombre : String
+    // descripcion: String, 
+    // reconocimientoOficial: Date, // Fecha
+    // dependencia: String, // Subvencionado, Municipal, etc
+    // sostenedor: String, // Corporacion o Sociedad sostenedora
+    // orientacionReligiosa: String,
+    // direccion: {
+    //     calle: String,
+    //     region: String,
+    //     comuna: String
+    // },
+    // correo: String,
+    // telefono: String,
+    // web: String,
+    // director: String,
+    // fechaPostulacion:{
+    //     inicio: Date,
+    //     cierre: Date
+    // },
+    // mensualidad: Number,
+    // totalAlumnosMatriculados: Number,
+    // promAlumnosCurso: Number,
+    // puntajeSimce: Number,
+    // proyectosEducativos: Array,
+    //nivelEnsenanza: [], // Media, Basica, PreKinder, etc
+    //usuarios: [],
+    //cursos: []
 });
 
 module.exports = mongoose.model('Organizacion', OrganizacionSchema);
--- app.js
+++ app.js
@@ -1,24 +1,7 @@
 'use strict';
-
-// const bodyParser = require('body-parser');
-// const path = require('path');
-
-// const mongoose = require('mongoose');
-
-// const Schema = mongoose.Schema;
-// const model = require('./api/models/organizacion');
-
-
-// var organizacionSchema = new Schema(model);
-// var Organizacion = mongoose.model("Organizacion",organizacionSchema);
-
-
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
 module.exports = app; // for testing
-
-// app.use(bodyParser.urlencoded({extended: false }));
-// app.use(bodyParser.json());
 
 var config = {
   appRoot: __dirname // required config
@@ -34,15 +17,26 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   app.listen(port);
 
   if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+    console.log('try this:\ncurl http://127.0.0.1/:' + port + '/hello?name=Scott');
   }
   if (swaggerExpress.runner.swagger.paths['/users']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/users?name=Scott');
+    console.log('try this:\ncurl http://127.0.0.1/:' + port + '/users?name=Scott');
   }
 });
 
-// // ruta de la pagina HOME
-// app.get('/', (req, res) => res.send('Hello World!'));
+var ModelOrganizacion = require('./api/models/Organizacion');
 
-// // Coneccion con mongoose
-// mongoose.connect('mongodb://localhost/rafiki', { useMongoClient: true })
+app.get('/get', (req, res) => {
+  ModelOrganizacion.find({}, (err, organizacion) => {
+      console.log(organizacion.length);
+      if(err) return res.status(500).send({message: 'Error al realizar peticion: ${err}'});
+      if(!organizacion) return res.status(400).send({message: 'No existe ninguna organizacion'});
+
+      res.status(200).send({organizacion});
+  });
+});
+
+
+app.get('/', (req, res) => {
+  res.send('Hola');
+});
