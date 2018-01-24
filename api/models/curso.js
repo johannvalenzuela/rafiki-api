a66bf46b91eb538f64c6c396af59fc888f319d7d
scarrascofuentes <samuel.carrasco.f@mail.pucv.cl>
2018-01-23 21:26:54 -0300

    Revert "Se crea ruta /cursos"

--- api/models/curso.js
+++ api/models/curso.js
@@ -10,8 +10,6 @@ var database = 'admin';
 var port = '27017';
 var host = '54.233.193.162';
 
-
-
 mongoose.connect(`mongodb://${username}:${password}@${host}:${port}/${database}`, (err, res) => {
     if(err) {
         return console.log(`Error al conectarse a la BD: ${err}`);
