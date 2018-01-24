891d9e9c3c6266878fd1ab63a44d0012e4988c19
Unknown <by.darklancer@gmail.com>
2018-01-24 16:25:08 -0300

    modificando coneccion al servidor

--- api/models/asignatura.js
+++ api/models/asignatura.js
@@ -1,30 +1,11 @@
 'use strict';
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-// realizando conexion con la bd
-var username = 'Rafiki';
-var password = encodeURIComponent('#Zeus2018');
-var database = 'admin';
-var port = '27017';
-var host = '54.233.193.162';
-
-
-// Añadir conexion con MongoDB aqui
-mongoose.connect(`mongodb://${username}:${password}@${host}:${port}/${database}`, (err, res) => {
-    if(err) {
-        return console.log(`Error al conectarse a la BD: ${err}`);
-    }
-    console.log('Conexion con la BD OK...!');
-});
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema
 
 const AsignaturaSchema = new Schema ({
     //id: String, // Codigo de cada establecimiento
     nombre: String,
-    enfasis: {
-        type: [{
-            type: String
-        }]
-    },//descripcion: String,
+    enfasis:[],
     horasPedagogicasConJecAnual: Number,
     horasPedagogicasSinJecAnual: Number,
     horasPedagogicasConJecSemanal: Number,
--- app.js
+++ app.js
@@ -2,18 +2,21 @@
 
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
+var mongoose = require('mongoose');
+require("./api/models/user.js");
 module.exports = app; // for testing
 
 var config = {
   appRoot: __dirname // required config
 };
 
-SwaggerExpress.create(config, function(err, swaggerExpress) {
+SwaggerExpress.create(config, (err, swaggerExpress) => {
   if (err) { throw err; }
 
   // install middleware
   swaggerExpress.register(app);
-
+  mongoose.Promise = global.Promise;
+  mongoose.connect('mongodb://54.233.193.162:27017/rafiki-test');
   var port = process.env.PORT || 10010;
   app.listen(port);
 
