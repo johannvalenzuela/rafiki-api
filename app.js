e0895c4c598d7533045160461a30832e9764bb9d
scarrascofuentes <samuel.carrasco.f@mail.pucv.cl>
2018-01-24 21:52:33 -0300

    Se implementa nueva conexión a base de datos
    La nueva conexión se implementa en el archivo app.js. Se utiliza una nueva base de datos: rafiki-test

--- app.js
+++ app.js
@@ -3,6 +3,22 @@
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
 module.exports = app; // for testing
+const mongoose = require('mongoose');
+
+var username = 'Rafiki';
+var password = encodeURIComponent('#Zeus2018');
+var database = 'rafiki-test';
+var port = '27017';
+var host = '54.233.193.162';
+
+mongoose.connect(`mongodb://${host}:${port}/${database}`, (err, res) => {
+    if(err) {
+        return console.log(`Error al conectarse a la BD: ${err}`);
+    }
+    console.log('Conexion con la BD OK...!');
+});
+
+
 
 var config = {
   appRoot: __dirname // required config
