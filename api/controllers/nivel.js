e093c16e2e43a41d73c96e22157ac6dc6e37d9da
Héctor Astorga <hm.astorga@gmail.com>
2018-01-26 13:16:46 -0300

    Style: se optimiza el código
    se eliminan lineas innecesarias en los condicionales if.

--- api/controllers/nivel.js
+++ api/controllers/nivel.js
@@ -32,16 +32,10 @@ function getNivelId(req, res) {
    /** FindById, busca dentro de la BD el nivel que se desea conseguir*/
   ModelNivel.findById(id, function(err, nivel){
       /** Si existe un error interno del servidor se retorna error 500 */
-    if (err){
-     
-      res.status(500).send(Responses.getError({message: err.message}));
-      return;
-    }
+    if (err) return res.status(500).send(Responses.getError({message: err.message}));
+
     /** Si no existe el nivel se retorna error 404 */
-    if (!nivel){
-      res.status(404).send(Responses.getError({message: `nivel ${id} not found.`}));
-      return;
-    }
+    if (!nivel) return res.status(404).send(Responses.getError({message: `nivel ${id} not found.`}));
 
     res.json(nivel);
 });
@@ -54,10 +48,7 @@ function createNivel(request, response) {
     nivel.save(function(err){
 
       /** Si existe un error interno del servidor se retorna error 500 */
-      if (err){
-        response.status(500).send(Responses.getError({message: err.message}));
-        return;
-      }
+      if (err) return response.status(500).send(Responses.getError({message: err.message}));
       console.log(nivel);
 
       /** Se crea el nuevo nivel con los atributos requeridos */
@@ -76,23 +67,20 @@ function updateNivel(request, response) {
   let id = request.swagger.params.id.value;
   /** FindById, busca dentro de la BD el nivel que se desea actualizar */
   ModelNivel.findById(id, function(err, nivel) {
-    if (err) {
-      response.status(500).send(Responses.getError({message: err.message}));
-      return;
-    }
+    /** Si existe un error interno del servidor se retorna error 500 */
+    if (err) return response.status(500).send(Responses.getError({message: err.message}));
+      
+    
     /** Si el id no está asociado a algún nivel en la BD, se retorna un error 404 */
-    if (!nivel) {
-      response.status(404).send(Responses.getError({message: `nivel ${id} not found.`}));
-      return;
-    }
+    if (!nivel) return response.status(404).send(Responses.getError({message: `nivel ${id} not found.`}));
+      
     /** Object.assign copia los valores del request.body al objeto nivel */
     nivel = Object.assign(nivel, request.body);
     nivel.save(id, function (err, nivel) {
-      if (err) {
-        /** si hay error al guardar se muestra un error 500 */
-        response.status(500).send(Responses.getError({message: err.message}));
-      }
-
+      /** si hay error al guardar se muestra un error 500 */
+      if (err) return response.status(500).send(Responses.getError({message: err.message}));
+      
+      /** Se muestra el nivel actualizado */
       response.json(nivel);
     });
   });
@@ -101,23 +89,16 @@ function updateNivel(request, response) {
 function deleteNivel(request, response){
   let id = request.swagger.params.id.value;
   ModelNivel.findById(id, function(err, nivel) {
-    if (err) {
-      response.status(500).send(Responses.getError({message: err.message}));
-      return;
-    }
+    if (err) return response.status(500).send(Responses.getError({message: err.message}));
+      
+    
     /** Si el id no está asociado a algún nivel en la BD, se retorna un error 404 */
-    if (!nivel) {
-      response.status(404).send(Responses.getError({message:  `El nivel ${id} no ha sido encontrado.`}));
-      return;
-    }
-    /** Se elimina de la BD el nivel asociado al id ingresado.
-     * Si existe un error de conexión se manda el error 500
-     * status(200) si todo salió bien.
-     */
+    if (!nivel) return response.status(404).send(Responses.getError({message:  `El nivel ${id} no ha sido encontrado.`}));
+      
+    /** Se elimina de la BD el nivel asociado al id ingresado. */
     nivel.remove(id, function (err, nivel) {
-      if (err) {
-        response.status(500).send(Responses.getError({message: err.message}));
-      }
+      if (err) return response.status(500).send(Responses.getError({message: err.message}));
+     /* Si no hay error se elimina y se muestra un mensaje de que el nivel ha sido borrado */ 
       response.status(200).json(Responses.getSuccess({message: `Nivel ${id} eliminado.`}));
     });
   });
