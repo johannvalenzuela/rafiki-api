155f9694aed7487b994ff4e0a67ceb936904bc3c
Johann <johann.aevt@gmail.com>
2018-01-30 11:33:39 -0300

    feat: Integrando JWT

---
+++ api/controllers/login.js
@@ -0,0 +1,28 @@
+var auth = require("../helpers/auth");
+
+module.exports = {
+    loginPost: loginPost
+};
+
+function loginPost(req, res, next){
+    var role = req.swagger.params.role.value;
+    var username = req.body.username;
+    var password = req.body.password;
+  
+    if (role != "user" && role != "admin") {
+      var response = { message: 'Error: Role must be either "admin" or "user"' };
+      res.writeHead(400, { "Content-Type": "application/json" });
+      return res.end(JSON.stringify(response));
+    }
+  
+    if (username == "username" && password == "password" && role) {
+      var tokenString = auth.issueToken(username, role);
+      var response = { token: tokenString };
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(JSON.stringify(response));
+    } else {
+      var response = { message: "Error: Credentials incorrect" };
+      res.writeHead(403, { "Content-Type": "application/json" });
+      return res.end(JSON.stringify(response));
+    }
+  };
