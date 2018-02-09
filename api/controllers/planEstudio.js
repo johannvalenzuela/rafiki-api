f9b7897c5bd2ffa19b7ebb5c1ad11507ded3cb42
Unknown <ogasvega.israel@gmail.com>
2018-02-09 19:42:45 -0300

    style: se cambiaron los comentarios

--- api/controllers/planEstudio.js
+++ api/controllers/planEstudio.js
@@ -5,12 +5,14 @@ var util = require('util');
 const ModelPlanEstudio = require('../../api/models/planEstudio');
 const Responses = require('../helpers/responses');
 
-/*
-  Funcion que obtiene una lista de todas las planEstudioes
-
-  Param 1: una identificacion de solicitud de un objeto
-  Param 2: una identificacion de respuesta de un objeto
-*/
+/** 
+ * @name getPlanEstudios getPlanEstudios GET /planEstudio
+ * @description Funcion que obtiene una lista de todas las planEstudioes
+ * @author Israel Ogas
+ * @param req una identificacion de solicitud de un objeto
+ * @param res una identificacion de respuesta de un objeto
+ * @return {planEstudio} JSON Objeto planEstudio
+ */
 exports.getPlanEstudios = (req, res) => {
     ModelPlanEstudio.find({}, (err, planEstudio) => {
         if (err) return res.status(500).send({ message: "Error al realizar peticion: ${err}" });
@@ -22,14 +24,17 @@ exports.getPlanEstudios = (req, res) => {
     });
 }
 
-/*
-  Funcion que obtiene un planEstudio en especifico por medio de la ID
-
-  Param 1: una identificacion de solicitud de un objeto
-  Param 2: una identificacion de respuesta de un objeto
-*/
+/** 
+ * @name getPlanEstudio getPlanEstudio GET /planEstudio
+ * @description Funcion que obtiene un plan de estudio en especifico por medio de la ID
+ * @author Israel Ogas
+ * @param req una identificacion de solicitud de un objeto
+ * @param res una identificacion de respuesta de un objeto
+ * @return {planEstudio} JSON Objeto planEstudio
+ */
 exports.getPlanEstudio = (req, res) => {
     let planEstudioID = req.swagger.params.id.value;
+
     ModelPlanEstudio.findById(planEstudioID, function (err, planEstudio) {
         // console.log(planEstudio);
         if (err) return res.status(500).send(Responses.getError({ message: err.message }));
@@ -39,18 +44,35 @@ exports.getPlanEstudio = (req, res) => {
     });
 }
 
-/*
-  Funcion que actualiza los atributos modificados de una planEstudio en especifico por medio de la ID
+/** 
+ * @name postPlanEstudio postPlanEstudio POST /planEstudio
+ * @description Funcion que crea un plan de estudio
+ * @author Israel Ogas
+ * @param req una identificacion de solicitud de un objeto
+ * @param res una identificacion de respuesta de un objeto
+ * @return {planEstudio} JSON Objeto planEstudio
+ */
+exports.postPlanEstudio = (req, res) => {
+    ModelPlanEstudio.create(req.body, function (err, planEstudio) {
+        planEstudio.save(function (err) {
+            if (err) return res.status(500).send(Responses.getError({ message: err.message }));
+            res.status(200).json(planEstudio);
+        })
+    });
+}
 
-  Param 1: una identificacion de solicitud de un objeto
-  Param 2: una identificacion de respuesta de un objeto
-*/
+/** 
+ * @name updatePlanEstudio updatePlanEstudio PUT /planEstudio
+ * @description Funcion que actualiza los atributos modificados de un plan de estudio por medio de la ID
+ * @author Israel Ogas
+ * @param req una identificacion de solicitud de un objeto
+ * @param res una identificacion de respuesta de un objeto
+ * @return {planEstudio} JSON Objeto planEstudio
+ */
 exports.updatePlanEstudio = (req, res) => {
     let id = req.swagger.params.id.value;
 
-    if (id.length < 24 || id.length > 24) {
-        return res.status(400).send(Responses.getError({ message: 'Se ingresó una ID no valida' }));
-    }
+    if (id.length < 24 || id.length > 24) return res.status(400).send(Responses.getError({ message: 'Se ingresó una ID no valida' }));
 
     ModelPlanEstudio.findById(id, function (err, planEstudio) {
         if (err) return res.status(500).send(Responses.getError({ message: err.message }));
@@ -64,45 +86,27 @@ exports.updatePlanEstudio = (req, res) => {
     });
 }
 
-
-/*
-  Funcion que elimina una planEstudio en especifico por medio de la ID
-
-  Param 1: una identificacion de solicitud de un objeto
-  Param 2: una identificacion de respuesta de un objeto
-*/
+/** 
+ * @name deletePlanEstudio deletePlanEstudio DELETE /planEstudio
+ * @description Funcion que elimina un plan de estudio especifico por medio de la ID
+ * @author Israel Ogas
+ * @param req una identificacion de solicitud de un objeto
+ * @param res una identificacion de respuesta de un objeto
+ * @return {planEstudio} JSON Objeto planEstudio
+ */
 exports.deletePlanEstudio = (req, res) => {
     let planEstudioID = req.swagger.params.id.value;
-    console.log(planEstudioID);
 
-    if (planEstudioID.length < 24 || planEstudioID.length > 24) {
-        return res.status(400).send(Responses.getError({ message: 'Se ingresó una ID no valida' }));
-    }
+    if (planEstudioID.length < 24 || planEstudioID.length > 24) return res.status(400).send(Responses.getError({ message: 'Se ingresó una ID no valida' }));
 
     ModelPlanEstudio.findById(planEstudioID, (err, planEstudio) => {
         if (err) return res.status(500).json({ message: `Error al borrar la plan de estudio: ${err}` });
         if (!planEstudio) return res.status(404).send(Responses.getError({ message: `El plan de estudio de ID ${planEstudioID} no existe.` }));
 
-        //Elimina la planEstudio si se encontró el id
+        //Elimina un plan de estudio si se encontró el id
         planEstudio.remove(planEstudioID, function (err, planEstudio) {
             if (err) return res.status(500).send(Responses.getError({ message: err.message }));
-            res.status(200).json(Responses.getSuccess({ message: `El plan de estudio ${planEstudioID} ha sido borrada.` }));
+            res.status(200).json(Responses.getSuccess({ message: `El plan de estudio ${planEstudioID} ha sido borrado.` }));
         });
-    });
-}
-
-
-/*
-  Funcion que crea una planEstudio
-
-  Param 1: una identificacion de solicitud de un objeto
-  Param 2: una identificacion de respuesta de un objeto
-*/
-exports.postPlanEstudio = (req, res) => {
-    ModelPlanEstudio.create(req.body, function (err, planEstudio) {
-        planEstudio.save(function (err) {
-            if (err) return res.status(500).send(Responses.getError({ message: err.message }));
-            res.status(200).json(planEstudio);
-        })
     });
 }
