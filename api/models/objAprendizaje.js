f445711638311ddbb433ef1ed7a9290b4686a67a
Unknown <by.darklancer@gmail.com>
2018-02-09 13:30:38 -0300

    feat: modelo "objAprendizaje"

---
+++ api/models/objAprendizaje.js
@@ -0,0 +1,14 @@
+'use strict';
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const objAprendizaje = new Schema ({
+    numero: Number,
+    tipo: {
+        type: String, 
+        enum: ['Habilidad', 'Conocimiento', 'Actitud']
+    },
+    descripcion: String
+});
+
+module.exports = mongoose.model('ObjAprendizaje', objAprendizajeSchema);
