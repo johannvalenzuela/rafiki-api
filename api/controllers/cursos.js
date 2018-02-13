40db27b1c0791d8d52b3cd1ac46e87d49d870ed4
scarrascofuentes <samuel.carrasco.f@mail.pucv.cl>
2018-02-13 17:01:32 -0300

    style: Se da un nuevo formato al controlador Curso
    - Se cambia el estilo de comentarios
    - Se cambia el estilo de exportación de funciones

--- api/controllers/cursos.js
+++ api/controllers/cursos.js
@@ -1,188 +1,149 @@
-'use strict';
-/*
- 'use strict' is not required but helpful for turning syntactical errors into true errors in the program flow
- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
-*/
-
-/*
- Modules make it possible to import JavaScript files into your application.  Modules are imported
- using 'require' statements that give you a reference to the module.
-
-  It is a good idea to list the modules that your application depends on in the package.json in the project root
- */
 var util = require('util');
 var ModelCurso = require('../../api/models/curso');
 const Responses = require('../helpers/responses');
 
-/*const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());*/
 
-/*
- Once you 'require' a module you can reference the things that it exports.  These are defined in module.exports.
-
- For a controller in a127 (which this is) you should export the functions referenced in your Swagger document by name.
-
- Either:
-  - The HTTP Verb of the corresponding operation (get, put, post, delete, etc)
-  - Or the operationId associated with the operation in your Swagger document
-
-  In the starter/skeleton project the 'get' operation on the '/hello' path has an operationId named 'hello'.  Here,
-  we specify that in the exports of this module that 'hello' maps to the function named 'hello'
+/** 
+ * Función para obtener un arreglo de cursos
+ *
+ * @author Samuel Carrasco Fuentes
+ * @exports getCursos GET /cursos
+ * @param req Petición HTTP
+ * @param res | 200 Cursos | 404 No hay cursos | 500 Error al buscar |
+ * @return {[cursos]} JSON con un objeto que contiene arreglo de Objetos Curso
  */
-module.exports = {
-  getCursos: getCursos,
-  getCursoId: getCursoId,
-  updateCurso: updateCurso,
-  deleteCurso: deleteCurso,
-  postCurso: postCurso,
+exports.getCursos = (req, res) => {
 
-};
-
-/*
-  Functions in a127 controllers used for operations should take two parameters:
-
-  Param 1: a handle to the request object
-  Param 2: a handle to the response object
- */
-
- //API REST: GET (Muestra Todos los cursos)
-
-function getCursos(req, res) {
-
-  /**Se buscan todos los cursos */
   ModelCurso.find({}, (err, cursos) => {
-    
-    /**En caso de error del servidor, se retorna error 500*/
-    if(err) return res.status(500).send({message: "Error al realizar peticion: ${err}"});
 
-    /**En caso que los cursos no se encuentren, se retorna un error 404*/ 
-    if(!cursos) return res.status(404).send({message: 'No existe ningún curso'});
+    if (err) return res.status(500).send({ message: `Error al realizar peticion: ${err}` });
 
-    /**Se responde todos los cursos*/
-    res.status(200).send({cursos});
-});
-}
+    if (!cursos) return res.status(404).send({ message: 'No existe ningún curso' });
 
-
-//API REST: GET (Muestra un curso según Id)
-
-function getCursoId(req, res) {
-  
-  /**Se recibe y guarda el id del curso */
-  let cursoId = req.swagger.params.id.value
-
-  /**Se busca el curso mediante id */
-  ModelCurso.findById(cursoId, (err, curso) => {
-    
-    /**En caso de error del servidor, se retorna error 500*/
-    if(err) return res.status(500).send({message: 'Error al realizar peticion: ${err}'});
-
-    /**En caso que el curso no se encuentre, se retorna un error 404*/ 
-    if(!curso) return res.status(404).send({message: 'El curso no existe'});
-
-    /**Se responde el curso según id */
-    res.status(200).send({curso : curso});
-  
+    res.status(200).send({ cursos });
   });
 }
 
-//API REST: DELETE (Elimina un curso según Id)
+/** 
+ * Función para obtener un curso.
+ *
+ * @author Samuel Carrasco Fuentes
+ * @exports getCurso GET /cursos/{id}
+ * @param req Petición HTTP, id de curso en path
+ * @param res | 200 Curso encontrado | 404 Curso no existe | 500 Error al buscar |
+ * @return {curso: curso} JSON con un Objeto Curso
+ */
+exports.getCurso = (req, res) => {
 
-function deleteCurso(request, response) {
-  
-  /**Se recibe y guarda el id del curso */
-  let cursoID = request.swagger.params.id.value;
-  
-  /**Se busca el curso mediante id */
-  ModelCurso.findById(cursoID, (err, curso) => {
+  let idCurso = req.swagger.params.id.value
 
-    /**En caso de error del servidor, se retorna error 500*/
-    if(err) return response.status(500).json({message: `Error al borrar el curso. Error: ${err}`});
-    
-    /**En caso que el curso no se encuentre, se retorna un error 404*/ 
+  ModelCurso.findById(idCurso, (err, curso) => {
+
+    if (err) return res.status(500).send({ message: `Error al realizar peticion: ${err}` });
+
+    if (!curso) return res.status(404).send({ message: 'El curso no existe' });
+
+    res.status(200).send({ curso: curso });
+
+  });
+}
+
+/** 
+ * Función para eliminar un curso.
+ *
+ * @author Samuel Carrasco Fuentes
+ * @exports deleteCurso DELETE /cursos/{id}
+ * @param req Petición HTTP, id de curso en Path
+ * @param res | 200 curso eliminado | 500 Error al buscar | 404 El curso no existe |
+ * @return {message:mensaje} JSON con mensaje
+ */
+exports.deleteCurso = (req, res) => {
+
+  let idCurso = req.swagger.params.id.value;
+
+  ModelCurso.findById(idCurso, (err, curso) => {
+
+    if (err) return res.status(500).json({ message: `Error al borrar el curso. Error: ${err}` });
+
     if (!curso) {
-      response.status(404).send(Responses.getError({message:  `El curso de ID ${cursoID} no existe`}));
+      res.status(404).send(Responses.getError({ message: `El curso de ID ${idCurso} no existe` }));
       return;
     }
 
-    /**Se elimina el curso */
-    curso.remove(cursoID, function (err, curso) {
+    curso.remove(idCurso, function (err, curso) {
       if (err) {
-        response.status(500).send(Responses.getError({message: err.message}));
+        res.status(500).send(Responses.getError({ message: err.message }));
         return;
       }
-      response.status(200).json(Responses.getSuccess({message: `El curso ${cursoID} ha sido eliminado`}));
+
+      res.status(200).json(Responses.getSuccess({ message: `El curso ${idCurso} ha sido eliminado` }));
     });
   });
 }
 
-//API REST: PUT (Actualiza un curso según Id)
+/** 
+ * Función para actualizar un curso.
+ *
+ * @author Samuel Carrasco Fuentes
+ * @exports updateCurso PUT /cursos/{id}
+ * @param req Petición HTTP, id de curso en path
+ * @param res | 200 Curso encontrado | 404 Evaluación no existe | 500 Error al buscar |
+ * @return {curso} JSON con Objeto Curso
+ */
+exports.updateCurso = (req, res) => {
 
-function updateCurso(request, response) {
-  
-  /**Se recibe y guarda el id del curso */
-  let idCurso = request.swagger.params.id.value;
- 
-  /**Se busca el curso mediante id */
-  ModelCurso.findById(idCurso, function(err, curso) {
-    
-    /**En caso de error del servidor, se retorna error 500*/
+
+  let idCurso = req.swagger.params.id.value;
+
+
+  ModelCurso.findById(idCurso, function (err, curso) {
+
+
     if (err) {
-      response.status(500).send(Responses.getError({message: err.message}));
+      res.status(500).send(Responses.getError({ message: err.message }));
       return;
     }
 
-    /**En caso que el curso no se encuentre, se retorna un error 404*/ 
     if (!curso) {
-      response.status(404).send(Responses.getError({message: 'El Curso ${idCurso} no existe'}));
+      res.status(404).send(Responses.getError({ message: `El Curso ${idCurso} no existe` }));
       return;
     }
-    
-    /**Se copian los valores al curso */
-    curso = Object.assign(curso, request.body);
-    
-    /**Se guardan los nuevos valores del curso */
+
+    curso = Object.assign(curso, req.body);
+
     curso.save(idCurso, function (err, curso) {
 
       if (err) {
-        response.status(500).send(Responses.getError({message: err.message}));
+        res.status(500).send(Responses.getError({ message: err.message }));
         return;
       }
 
-       /**Se responde json con el contenido actualizado del curso */
-      response.json(curso);
+      res.json(curso);
     });
   });
 }
 
-//API REST: POST (Inserta un nuevo curso)
+/** 
+ * Función para insertar un curso.
+ *
+ * @author Samuel Carrasco Fuentes
+ * @exports postcurso POST /cursos
+ * @param req Petición HTTP, JSON con Objeto curso en Body
+ * @param res | 200 Curso creado | 500 Error al buscar |
+ * @return {curso} JSON con Objeto Curso
+ */
+exports.postCurso = (req, res) => {
 
-function postCurso(request, response) {
+  ModelCurso.create(req.body, function (err, curso) {
 
-  /**Se crea un nuevo curso */
-  ModelCurso.create(request.body, function (err, curso) {
-    
-    /**Se guarda el nuevo curso */
-    curso.save(function(err){
-      
-      /**En caso de error del servidor, se retorna error 500*/
-      if (err){
-        response.status(500).send(Responses.getError({message: err.message}));
+    curso.save(function (err) {
+
+      if (err) {
+        res.status(500).send(Responses.getError({ message: err.message }));
         return;
       }
 
-      /**Se responde un json con el contenido del curso*/
-      response.status(200).json({ 
-        idCurso : curso.idCurso,
-        nivel : curso.nivel,
-        asignatura : curso.asignatura,
-        profesorJefe : curso.profesorJefe,
-        salaCurso : curso.salaCurso,
-        totalAlumnos: curso.totalAlumnos
-      });
+      res.status(200).json(curso);
     })
   });
 }
