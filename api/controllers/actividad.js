e397bf9a6c2f7e19a1c3ce95cf933daa49bfe61c
scarrascofuentes <samuel.carrasco.f@mail.pucv.cl>
2018-02-13 16:09:20 -0300

    style: Se da un nuevo formato al controlador Actividad
    - Se cambia el estilo de comentarios
    - Se cambia el estilo de exportación de funciones

--- api/controllers/actividad.js
+++ api/controllers/actividad.js
@@ -1,212 +1,165 @@
 'use strict';
-/*
- 'use strict' is not required but helpful for turning syntactical errors into true errors in the program flow
- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
-*/
 
-/*
- Modules make it possible to import JavaScript files into your application.  Modules are imported
- using 'require' statements that give you a reference to the module.
-
-  It is a good idea to list the modules that your application depends on in the package.json in the project root
- */
 var util = require('util');
 var ModelActividad = require('../../api/models/actividad');
 const Responses = require('../helpers/responses');
 
-/*const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());*/
 
-/*
- Once you 'require' a module you can reference the things that it exports.  These are defined in module.exports.
-
- For a controller in a127 (which this is) you should export the functions referenced in your Swagger document by name.
-
- Either:
-  - The HTTP Verb of the corresponding operation (get, put, post, delete, etc)
-  - Or the operationId associated with the operation in your Swagger document
-
-  In the starter/skeleton project the 'get' operation on the '/hello' path has an operationId named 'hello'.  Here,
-  we specify that in the exports of this module that 'hello' maps to the function named 'hello'
+/** 
+ * Función para obtener un arreglo de Actividades
+ *
+ * @author Samuel Carrasco Fuentes
+ * @exports getActividades GET /actividades
+ * @param req Petición HTTP
+ * @param res | 200 Actividades | 404 No hay actividades | 500 Error al buscar |
+ * @return {[actividades]} JSON con un objeto que contiene arreglo de Objetos Actividad
  */
-module.exports = {
-  getActividades: getActividades,
-  getActividadId: getActividadId,
-  updateActividad: updateActividad,
-  deleteActividad: deleteActividad,
-  postActividad: postActividad,
+exports.getActividades = (req, res) => {
 
-};
-
-/*
-  Functions in a127 controllers used for operations should take two parameters:
-
-  Param 1: a handle to the request object
-  Param 2: a handle to the response object
- */
-
- //API REST: GET (Muestra Todas las Actividades)
-
-function getActividades(req, res) {
-
-  /**Se buscan todas las Actividades */
   ModelActividad.find({}, (err, actividades) => {
+
+    if (err) return res.status(500).send({ message: `Error al realizar peticion: ${err}` });
+
+    if (!actividades) return res.status(404).send({ message: 'No existe ningún Actividad' });
     
-    /**En caso de error del servidor, se retorna error 500*/
-    if(err) return res.status(500).send({message: "Error al realizar peticion: ${err}"});
-
-    /**En caso que las Actividades no se encuentren, se retorna un error 404*/ 
-    if(!actividades) return res.status(404).send({message: 'No existe ningún Actividad'});
-
-    /**Se responde todos los Actividads*/
     console.log(actividades);
-    res.status(200).send({actividades});
-});
-}
 
-
-//API REST: GET (Muestra una actividad según Id)
-
-function getActividadId(req, res) {
-  
-  /**Se recibe y guarda el id de la Actividad */
-  let actividadId = req.swagger.params.id.value
-
-  /**Se busca la Actividad mediante id */
-  ModelActividad.findById(actividadId, (err, actividad) => {
-    
-    /**En caso de error del servidor, se retorna error 500*/
-    if(err) return res.status(500).send({message: 'Error al realizar peticion: ${err}'});
-
-    /**En caso que la Actividad no se encuentre, se retorna un error 404*/ 
-    if(!actividad) return res.status(404).send({message: 'La Actividad no existe'});
-
-    /**Se responde la Actividad según id */
-    res.status(200).send({actividad : actividad});
-  
+    res.status(200).send({ actividades });
   });
 }
 
 
+/** 
+ * Función para obtener una Actividad.
+ *
+ * @author Samuel Carrasco Fuentes
+ * @exports getActividad GET /actividades/{id}
+ * @param req Petición HTTP, id de actividad en path
+ * @param res | 200 Actividad encontrada | 404 Actividad no existe | 500 Error al buscar |
+ * @return {actividad: actividad} JSON con objeto Actividad
+ */
 
-//API REST: DELETE (Elimina una Actividad según Id)
+exports.getActividad = (req, res) => {
 
-function deleteActividad(request, response) {
+
+  let idActividad = req.swagger.params.id.value
+
   
-  /**Se recibe y guarda el id de la Actividad */
-  let actividadID = request.swagger.params.id.value;
-  
-  /**Se busca la Actividad mediante id */
-  ModelActividad.findById(actividadID, (err, actividad) => {
+  ModelActividad.findById(idActividad, (err, actividad) => {
 
-    /**En caso de error del servidor, se retorna error 500*/
-    if(err) return response.status(500).json({message: `Error al borrar la Actividad. Error: ${err}`});
-    
-    /**En caso que la Actividad no se encuentre, se retorna un error 404*/ 
+ 
+    if (err) return res.status(500).send({ message: `Error al realizar peticion: ${err}` });
+
+
+    if (!actividad) return res.status(404).send({ message: `La Actividad no existe` });
+
+    res.status(200).send({ actividad: actividad });
+
+  });
+}
+
+
+/** 
+ * Función para eliminar una Actividad.
+ *
+ * @author Samuel Carrasco Fuentes
+ * @exports deleteActividad DELETE /actividades/{id}
+ * @param req Petición HTTP, id de Actividad en Path
+ * @param res | 200 Actividad eliminada | 500 Error al buscar | 404 La Actividad no existe |
+ * @return {message: mensaje} JSON con mensaje
+ */
+exports.deleteActividad = (req, res) => {
+
+  let idActividad = req.swagger.params.id.value;
+
+  ModelActividad.findById(idActividad, (err, actividad) => {
+
+    if (err) return res.status(500).json({ message: `Error al borrar la Actividad. Error: ${err}` });
+
     if (!actividad) {
-      response.status(404).send(Responses.getError({message:  `La Actividad de ID ${actividadID} no existe`}));
+      res.status(404).send(Responses.getError({ message: `La Actividad de ID ${idActividad} no existe` }));
       return;
     }
 
-    /**Se elimina la Actividad */
-    actividad.remove(actividadID, function (err, actividad) {
+    actividad.remove(idActividad, function (err, actividad) {
       if (err) {
-        response.status(500).send(Responses.getError({message: err.message}));
+        res.status(500).send(Responses.getError({ message: err.message }));
         return;
       }
-      response.status(200).json(Responses.getSuccess({message: `La Actividad ${actividadID} ha sido eliminada`}));
+      res.status(200).json(Responses.getSuccess({ message: `La Actividad ${idActividad} ha sido eliminada` }));
     });
   });
 }
 
-//API REST: PUT (Actualiza una Actividad según Id)
+/** 
+ * Función para actualizar una Actividad.
+ *
+ * @author Samuel Carrasco Fuentes
+ * @exports updateActividad PUT /actividades/{id}
+ * @param req Petición HTTP, id de Actividad en path
+ * @param res | 200 Actividad encontrada | 404 Actividad no existe | 500 Error al buscar |
+ * @return {actividad} JSON Objeto Actividad
+ */
+exports.updateActividad = (req, res) => {
 
-function updateActividad(request, response) {
-  
-  /**Se recibe y guarda el id de la Actividad */
-  let idActividad = request.swagger.params.id.value;
- 
-  /**Se busca la Actividad mediante id */
-  ModelActividad.findById(idActividad, function(err, actividad) {
-    
-    /**En caso de error del servidor, se retorna error 500*/
+
+  let idActividad = req.swagger.params.id.value;
+
+  ModelActividad.findById(idActividad, function (err, actividad) {
+
+
     if (err) {
-      response.status(500).send(Responses.getError({message: err.message}));
+      res.status(500).send(Responses.getError({ message: err.message }));
       return;
     }
 
-    /**En caso que la Actividad no se encuentre, se retorna un error 404*/ 
+ 
     if (!actividad) {
-      response.status(404).send(Responses.getError({message: 'La Actividad ${idActividad} no existe'}));
+      res.status(404).send(Responses.getError({ message: `La Actividad ${idActividad} no existe` }));
       return;
     }
-    
-    /**Se copian los valores a la Actividad */
-    actividad = Object.assign(actividad, request.body);
-    
-    /**Se guardan los nuevos valores de la Actividad */
+
+    actividad = Object.assign(actividad, req.body);
+
     actividad.save(idActividad, function (err, Actividad) {
 
       if (err) {
-        response.status(500).send(Responses.getError({message: err.message}));
+        res.status(500).send(Responses.getError({ message: err.message }));
         return;
       }
 
-       /**Se responde json con el contenido actualizado de la Actividad */
-      response.json(actividad);
+      res.json(actividad);
     });
   });
 }
 
-//API REST: POST (Inserta una nueva Actividad)
+/** 
+ * Función para insertar una Actividad.
+ *
+ * @author Samuel Carrasco Fuentess
+ * @exports postActividad  POST /actividades
+ * @param req Petición HTTP, JSON Objeto Actividad en Body
+ * @param res | 200 Actividad creada | 500 Error al buscar |
+ * @return {actividad} JSON Objeto Actividad 
+ */
 
-function postActividad(request, response) {
+exports.postActividad = (req, res) => {
 
-    /**Se crea una nueva actividad */
-  ModelActividad.create(request.body, function (err, actividad) {
-    
-    /**Se guarda la nueva actividad */
-    actividad.save(function(err){
-      
-      /**En caso de error del servidor, se retorna error 500*/
-      if (err){
-        response.status(500).send(Responses.getError({message: err.message}));
+  ModelActividad.create(req.body, function (err, actividad) {
+
+    actividad.save(function (err) {
+
+      if (err) {
+        res.status(500).send(Responses.getError({ message: err.message }));
         return;
       }
 
-      /**Se responde un json con el contenido de la actividad*/
+      res.status(200).json(actividad);
 
-      response.status(200).json(actividad);
-
-     
     })
   });
 }
 
-/**Se responde un json con el contenido de la Actividad*/
-
-      /*
-
-      response.status(200).json({ 
-        profesorAutor : actividad.profesorAutor,
-        anhoAcademico: actividad.anhoAcademico,
-        semestre: actividad.semestre,
-        nivelDificultad: actividad.nivelDificultad,
-        nivelAprendizaje: actividad.nivelAprendizaje,
-        tipoPregunta: actividad.tipoPregunta,
-        tema: actividad.tema,
-        subTema: actividad.subTema,
-        preguntaEnunciado: actividad.preguntaEnunciado,
-        preguntaaActividades: actividad.preguntaaActividades,
-        respuesta: actividad.respuesta, 
-        respuestaVerdaderoFalso: actividad.respuestaVerdaderoFalso, 
-        respuestaAlternativas: actividad.respuestaAlternativas, 
-        puntajeTotal: actividad.puntajeTotal,
-        retroalimentacion: actividad.retroalimentacion
-      });*/
 
 
 
