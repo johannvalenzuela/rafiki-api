5f7514d6e8e3ca4ec8d193131725927749c5513f
Unknown <by.darklancer@gmail.com>
2018-02-19 17:26:25 -0300

    feat: actualizado modelo "objAprendizaje"

--- api/models/objAprendizaje.js
+++ api/models/objAprendizaje.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const objAprendizajeSchema = new Schema ({
-    numero:{
-        type: Number
+    identificador:{
+        type: String
     },
     tipo: {
         type: String, 
@@ -13,6 +13,10 @@ const objAprendizajeSchema = new Schema ({
     descripcion:{
         type: String
     },
+    createAt: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 module.exports = mongoose.model('objaprendizajes', objAprendizajeSchema);
