7bab4e226dc0a765e9f6b6e7a318ec97bf0463e6
scarrascofuentes <samuel.carrasco.f@mail.pucv.cl>
2018-02-19 22:20:53 -0300

    feat: Se actualiza modelo de Curso

--- api/models/curso.js
+++ api/models/curso.js
@@ -1,18 +1,63 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const CursoSchema = new Schema ({
+const CursoSchema = new Schema({
 
-    idCurso: String, 
-    nivel: String, 
-    asignatura: String,
-    profesorJefe: String, 
-    salaCurso: String, 
-    totalAlumnos: Number
-    //asignaturas: []
-    //promAlumnosCurso: Number,
-    //alumnos: [], 
-   
+    nombre: {
+        type: String
+    },
+
+    salaCurso: {
+        type: String
+    },
+
+    nivel: {
+        type: String
+    },
+
+    detalles: {
+        type: String
+    },
+
+    planificacion: {
+        type: [{
+
+            oa: String, //REFERENCIAR OBJETIVO DE APRENDIZAJE
+            fecha: Date,
+            detalles: String,
+            recursos: {
+                type: [{
+                    type: String
+                    //REFERENCIAR RECURSO EDUCATIVO
+                }]
+            },
+        }]
+    },
+
+    profesores: {
+        type: [{
+            type: String
+            //REFERENCIAR USER
+        }]
+    },
+
+    alumnos: {
+        type: [{
+            type: String
+            //REFERENCIAR USER
+        }]
+    },
+
+    //Fecha de creación
+    createAt: {
+        type: Date,
+        default: Date.now
+    }
+
+    /*asignatura: {
+        type: String
+    },*/
+
 });
 
 module.exports = mongoose.model('Curso', CursoSchema);
