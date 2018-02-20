7824614f996607d15e2a8f23a12e91626fccfdd5
scarrascofuentes <samuel.carrasco.f@mail.pucv.cl>
2018-02-19 22:23:19 -0300

    feat: Se actualiza modelo de Actividad

--- api/models/actividad.js
+++ api/models/actividad.js
@@ -3,84 +3,76 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ActividadSchema = new Schema ({
-    
-    profesorAutor: {
-        type: String, 
+const ActividadSchema = new Schema({
+
+    autor: {
+        type: String,
+        //RELACIONAR CON USER
     },
 
-    anhoAcademico: {
-        type: Number,
-    },
-
-    semestre: {
-        type: Number,
-        enum: ['1','2']
-    },
-
-    nivelDificultad: {
-        type: String, 
-        enum: ['Alta','Media','Baja']
+    dificultad: {
+        type: String,
+        enum: ['Alta', 'Media', 'Baja']
     },
 
     nivelAprendizaje: {
-        type: String, 
-        enum: ['Recordar', 'Comprender', 'Aplicar','Analizar','Evaluar', 'Crear']
-    }, 
-
-    tipoPregunta:{
-        type: String, 
-        enum: ['Enumeracion', 'Conocimiento especifico', 'Ensayo','Verdadero y Falso','Alternativas',]
+        type: String,
+        enum: ['Recordar', 'Comprender', 'Aplicar', 'Analizar', 'Evaluar', 'Crear']
     },
 
-    asignatura: {
+    tipoPregunta: {
+        type: String,
+        enum: ['Enumeracion', 'Conocimiento especifico', 'Ensayo', 'Verdadero y Falso', 'Alternativas',]
+    },
+
+    /*asignatura: {
         type: String 
-    },
+    },*/
 
     tema: {
-        type: String 
+        type: String
     },
 
     subTema: {
-        type: String 
+        type: String
     },
 
     preguntaEnunciado: {
         type: String
     },
 
-    preguntaAlternativas: {
+    alternativas: {
         type: [{
-          type: String  
+            type: String
         }]
     },
 
     respuesta: {
         type: String
     },
-    
+
     respuestaVerdaderoFalso: {
         type: Boolean
     },
 
-    respuestaAlternativas: {
-        type: [{
-            type: String  
-        }]
-    }, 
-
     puntajeTotal: {
-        type: Number, 
+        type: Number,
     },
 
     //Enlaces de interes y/o información complementaria. Se entrega solo en preguntas de Ensayo.
     retroalimentacion: {
         type: [{
-            type: String  
+            type: String
         }]
-    }, 
+    },
 
-  
+    //Fecha de creación
+    createAt: {
+        type: Date,
+        default: Date.now
+    }
+
+
 
 });
 
